995ab132cb5d1dcf3d74445c079c8de91c19e9fb
raudel revas <c.sa.r@hotmail.com>
2018-09-17 17:27:51 -0500

    se arreglo para que se puede executar desde heroku

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var path    = require("path");
 //var index= require('public/camara');
 app.use(express.static('public'));
 
-app.get('/camara', function(req,res){
-  res.sendfile(__dirname + '/public/camara.html');
- }); 
- app.get('/', function(req,res){
-  res.sendfile(__dirname + '/public/index.html');
- }); 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'public/index.html'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
